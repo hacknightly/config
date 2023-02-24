c3d90c25f783bfbb5114328f88d1908b8e33f750
Darrell Banks <darrellbanks@Darrells-Mini.hsd1.co.comcast.net>
2023-02-24 13:16:22 -0700

    zsh

--- config.fish
+++ config.fish
@@ -1,2 +1,8 @@
 set -x PATH /usr/local/bin /opt/homebrew/bin ~/.asdf/shims $PATH
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/darrellbanks/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
